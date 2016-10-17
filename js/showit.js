94776b7a1fa690812cccd0e3ceb2cd05e322e2f0
brianqeep <brites009@gmail.com>
2016-10-17 18:12:53 +0300

    paragraph highlight

---
+++ js/showit.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  $("p#para1").click(function(){
+    $("p#para1").css({"border": "solid","border-color": "#000","border-width": "1px", "background-color": "#d0d0d0"});
+  });
+  $("p#para2").click(function(){
+    $("p#para2").css({"border": "solid","border-color": "#000","border-width": "1px", "background-color": "#d0d0d0"});
+  });
+  $("p#para3").click(function(){
+    $("p#para3").css({"border": "solid","border-color": "#000","border-width": "1px", "background-color": "#d0d0d0"});
+  });
+  $("p#para4").click(function(){
+    $("p#para4").css({"border": "solid","border-color": "#000","border-width": "1px", "background-color": "#d0d0d0"});
+  });
+  $("p#para5").click(function(){
+    $("p#para5").css({"border": "solid","border-color": "#000","border-width": "1px", "background-color": "#d0d0d0"});
+  });
+});
